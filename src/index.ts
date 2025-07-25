69c9bf499fdce558113561ff22659c6f28dc8ca8
Nassredean Nasseri <nassredean@gmail.com>
2025-07-25 13:52:07 -0400

    WIP - Add support for exporting to singer format

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { Reader } from './reader.js';
 export { toExport, buildStringFormatVariables, formatStrSafely, getIndexSafely } from './etl-utils.js';
+export { genSingerHeader, toSingerSchema, toSinger, deepConvertDatetimes, parseObjs, getCatalogSchema, parseDfCols } from './singer.js';
 export type { ExportOptions } from './etl-utils.js';
 export type { Catalog, CatalogStream, PolarsSchema } from './reader.js';
+export type { SingerSchema, SingerHeaderMap, TypeMapping } from './singer.js';
