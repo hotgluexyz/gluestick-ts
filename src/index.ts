d79447ae35ee36c59e0a782dde697dc37512e393
Key <keynarafaelp@gmail.com>
2025-10-27 17:30:07 -0400

    add new functions to the index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Reader } from './reader.js';
-export { toExport, buildStringFormatVariables, formatStrSafely, getIndexSafely } from './etl-utils.js';
+export { toExport, buildStringFormatVariables, formatStrSafely, getIndexSafely, snapshotRecords, readSnapshots, localizeDatetime } from './etl-utils.js';
 export { genSingerHeader, toSingerSchema, toSinger, deepConvertDatetimes, parseObjs, getCatalogSchema, parseDfCols } from './singer.js';
 export type { ExportOptions } from './etl-utils.js';
 export type { Catalog, CatalogStream, PolarsSchema } from './reader.js';
